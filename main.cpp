bbc17e0c8c15e24fa4550bfad0fe2a8d61953116
jbwaring <jean.baptiste.waring@icloud.com>
2020-03-11 23:45:57 -0400

    added TUI

--- main.cpp
+++ main.cpp
@@ -31,19 +31,29 @@ return 0;
 }
 
 int main_menu(){
-	int ans;
 	init_dialog(stdin, stdout);
 	// one contiguous block of 4 chars
-    char c[2][8]={
+    char c[6][20]={
         "option1",
-        "option2"
+        "description1",
+        "option2",
+        "description2",
+        "option3",
+        "description3"
       };
 
     // use a smart pointer
-    std::unique_ptr<char*[]> param(new char*[2]);
+    char* param[6];
     param[0] = &c[0][0];
     param[1] = &c[1][0];
-	ans = dialog_menu("Main Menu - Hospital Management System", "Select an option:", 100, 100, 50, 2, param.get());
+    param[2] = &c[2][0];
+    param[3] = &c[3][0];
+    param[4] = &c[4][0];
+    param[5] = &c[5][0];
+    char** ptr = &param[0];
+	int ans = dialog_menu("Main Menu - Hospital Management System", "Select an option: (you can use the mouse!)", 100, 100, 50, 3, ptr);
+	cout << endl << "answer: " << ans;
 	end_dialog();
+	
 	return ans;
 }
